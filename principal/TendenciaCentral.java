70d5b05f1ad50528acc85e0b1a07a4bec1367e7b
Aulas <Aulas>
2016-07-25 13:11:37 -0300

    Criada a Função de Cálculo de Média

--- principal/TendenciaCentral.java
+++ principal/TendenciaCentral.java
@@ -1,8 +1,45 @@
 package principal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TendenciaCentral {
 
 	public static void main(String[] args) {
 		
+		List<Float> numeros = new ArrayList<>();
+		
+		numeros.add(1.0f);
+		numeros.add(3.0f);
+		numeros.add(3.0f);
+		numeros.add(3.0f);
+		numeros.add(5.0f);
+		numeros.add(5.0f);
+		numeros.add(5.0f);
+		numeros.add(8.0f);
+		
+		calcularMedia(numeros);		
 	}
+	
+	// Média, mediana e moda
+	
+	public static void calcularMedia(List<Float> numeros) {
+		/* Média
+		 * Soma das valores / frequência total
+		 */
+		
+		float resultado = 0.0f;
+		
+		for (Float numero : numeros) {
+			// Passando pela lista de números para somá-los
+			resultado += numero;
+		}
+		
+		float resultadoFinal = resultado / numeros.size();
+		
+		System.out.println("A média dos números citados é - " + resultadoFinal);
+		
+		
+	}
+	
 }
